5057de4264a71aacaab09829b33fd52f07bcfed7
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-10-18 09:59:12 -0400

    Initial Commit

---
+++ CardSorting/CardSortingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSortingApp.swift
+//  CardSorting
+//
+//  Created by Quin Taylor on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct CardSortingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
